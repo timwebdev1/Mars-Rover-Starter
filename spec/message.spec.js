19883c7fcb154382fa54736ae8a4313cba7d699c
Timothy McDermott <tlmcdermott79@gmail.com>
2024-07-31 20:45:49 -0500

    passed test 4 and 4 passed npm test

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -1,9 +1,16 @@
 const Message = require('../message.js');
 const Command = require('../command.js');
 
-// NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
-//       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
-
 describe("Message class", function() {
+// TEST 4
+it("throws error if a name is NOT passed into the constructor as the first parameter", function() {
+  expect( function() { new Message();}).toThrow(new Error('Name required.'));
+});
+
+// TEST 5
+
+
+// TEST 6
+
 
 });
