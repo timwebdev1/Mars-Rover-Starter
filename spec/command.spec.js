e6388c92e12a445149a4a0f1b3a75d52e3c0acea
Timothy McDermott <tlmcdermott79@gmail.com>
2024-07-31 20:06:40 -0500

    passed test 2

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -5,10 +5,23 @@ describe("Command class", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
 
+  // TEST 2
+  it("constructor sets command type", function() {
+    const commandType = 'SOME_COMMAND_TYPE';
+    const command = new Command(commandType);
+    expect(command.commandType).toBe(commandType);
+  })
+
+// TEST 3
+// it("constructor sets a value passed in as the 2nd argument", function() {
+//   const value = 'SOME_VALUE';
+//   const command = new Command(value);
+//   expect(command.value).toBe(value);
+// })
+
 });
 
 
-// TEST 2
 
 
-// TEST 3
+
