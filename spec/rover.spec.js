0f3fec85909981acdc3256c612d35cc8de96c6e7
Timothy McDermott <tlmcdermott79@gmail.com>
2024-08-02 20:12:33 -0500

    constructed test 7

--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -5,7 +5,15 @@ const Command = require('../command.js');
 describe("Rover class", function() {
 
   // TEST 7
-
+it("constructor sets position and default values for mode and generatorWatts", function() {
+  const position = 12345;
+  const mode = "NORMAL";
+  const generatorWatts = 110;
+  const rover = new Rover(position, mode, generatorWatts);
+  expect(rover.position).toBe(position);
+  expect(rover.mode).toBe(mode);
+  expect(rover.generatorWatts).toBe(generatorWatts);
+})
   
   // TEST 8
 
