9dfcf7d079a1054be8b515c3759029e64b66e951
Timothy McDermott <tlmcdermott79@gmail.com>
2024-08-02 20:15:40 -0500

    passed test 7, npm tests: 7 passed

--- rover.js
+++ rover.js
@@ -1,5 +1,9 @@
 class Rover {
-   // Write code here!
+   constructor(position, mode = "NORMAL", generatorWatts = 110) {
+      this.position = position;
+      this.mode = mode;
+      this.generatorWatts = generatorWatts;
+   }
 }
 
 module.exports = Rover;
